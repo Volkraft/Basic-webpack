4ee1ee9d9b2f1e7566b558d970028183727fbfc3
Deniska,VOLKRAFT <volkraft9@gmail.com>
2024-06-25 10:36:08 +0300

    Feat: add prettier, eslint

--- src/index/index-entry.js
+++ src/index/index-entry.js
@@ -1,3 +1,4 @@
-import './index.html';
-import '../common/common-style/common-style.scss';
-import '../common/common-style/header.scss';
+import "./index.html";
+import "../common/common-style/common-style.scss";
+import "../common/common-style/header.scss";
+import "../index/index-style/index-style.scss";
