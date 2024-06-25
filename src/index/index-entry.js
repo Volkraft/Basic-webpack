241b9dedb66283eef686d3468c758c92dd1ac30b
Deniska,VOLKRAFT <volkraft9@gmail.com>
2024-06-25 11:12:12 +0300

    Feat: script for hidden cat

--- src/index/index-entry.js
+++ src/index/index-entry.js
@@ -2,3 +2,4 @@ import "./index.html";
 import "../common/common-style/common-style.scss";
 import "../common/common-style/header.scss";
 import "../index/index-style/index-style.scss";
+import "../index/index-script/index-script.js";
